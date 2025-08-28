473cb9faeb4ee6851342b74ebe5854499067258a
luanaportellarodriguez <l.portella.rodriguez@accenture.com>
2025-08-28 13:52:04 -0300

    feat(image): add facebook image

--- app/code/Luana/Sharer/Block/Sharer.php
+++ app/code/Luana/Sharer/Block/Sharer.php
@@ -2,15 +2,22 @@
 namespace Luana\Sharer\Block;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Sharer extends Template
 {
-    public function __construct(Template\Context $context, array $data = [], StoreManagerInterface $storeManager) {
+    private StoreManagerInterface $storeManager;
+
+    public function __construct(
+        Template\Context $context,
+        StoreManagerInterface $storeManager,
+        array $data = []
+    ) {
         parent::__construct($context, $data);
         $this->storeManager = $storeManager;
     }
 
     public function getCurrentUrl() {
-        return $this->storeManager->getStore()->getCurrentUrl();
+        return urldecode($this->storeManager->getStore()->getCurrentUrl());
     }
 }
